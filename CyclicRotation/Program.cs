08f63788bee230092ab3c2421d7e8e6bbf58d9a2
Yosheng <yosheng0323@outlook.com>
2020-09-29 12:30:02 +0800

    Solve CyclicRotation

---
+++ CyclicRotation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CyclicRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // [9, 7, 6, 3, 8]
+            var result1 = solution(new []{ 3, 8, 9, 7, 6}, 3);
+            Console.WriteLine("[{0}]", string.Join(", ", result1));
+            
+            //  [1, 2, 3, 4]
+            var result2 = solution(new []{ 1, 2, 3, 4}, 4);
+            Console.WriteLine("[{0}]", string.Join(", ", result2));
+        }
+
+        static int[] solution(int[] A, int K)
+        {
+            if (A.Length == K)
+            {
+                return A;
+            }
+
+            int[] result = new int[A.Length];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (i + K >= result.Length)
+                {
+                    result[(i + K) - result.Length] = A[i];
+                }
+                else
+                {
+                    result[i + K] = A[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}

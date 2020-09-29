37fabd9344463868eb9d1ebbd96780aeca76d4a0
Yosheng <yosheng0323@outlook.com>
2020-09-29 16:10:30 +0800

    Solve FrogJmp

---
+++ FrogJmp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FrogJmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(10,85,30));
+            
+            Console.WriteLine(solutionByMath(10,85,30));
+        }
+
+        static int solution(int X, int Y, int D)
+        {
+            int count = 0;
+
+            while (X + D * count <　Y)
+            {
+                count++;
+            }
+
+            return count;
+        }
+        
+        static int solutionByMath(int X, int Y, int D)
+        {
+            int count = 0;
+
+            count = (int)Math.Ceiling((double) (Y - X) / D);
+
+            return count;
+        }
+    }
+}

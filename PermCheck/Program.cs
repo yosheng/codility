2468c33fc2a6a7f50f6539639ba7725c1b3abaf3
Yosheng <sad445566@gmail.com>
2020-10-06 11:18:39 +0800

    Solve PermCheck

---
+++ PermCheck/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PermCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        static int solution(int[] A) {
+            var set = new HashSet<int>();
+            for (int i = 0; i < A.Length; i ++) {
+                if (A[i] > A.Length) return 0;
+                set.Add(A[i]);
+            }
+            return set.Count == A.Length ? 1 : 0;
+        }
+    }
+}

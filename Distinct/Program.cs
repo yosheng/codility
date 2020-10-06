b96d0aad35f9a8aa880af545c649942422d3976f
Yosheng <sad445566@gmail.com>
2020-10-06 21:28:01 +0800

    Solve Distinct

---
+++ Distinct/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Distinct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        static int solution(int[] A) {
+            var set = new HashSet<int>();
+        
+            for (int i = 0; i < A.Length; i++){
+                set.Add(A[i]);
+            }
+        
+            return set.Count;
+        }
+    }
+}

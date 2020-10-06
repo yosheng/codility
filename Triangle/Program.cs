2fd654381c16011ab93178bac27d7279c77884f1
Yosheng <sad445566@gmail.com>
2020-10-06 23:40:59 +0800

    Solve Triangle

---
+++ Triangle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        /// <summary>
+        /// 93%
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        static int solution(int[] A) {
+            Array.Sort(A);
+        
+            for(int i=0; i<A.Length-2; i++){
+                if (A[i]+A[i+1] > A[i+2]) return 1;
+            }
+        
+            return 0;
+        }
+    }
+}

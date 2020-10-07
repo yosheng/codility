13de5f36e919c03907ebc6dd35c4e743e4eb0b21
Yosheng <sad445566@gmail.com>
2020-10-07 15:43:31 +0800

    Solve StoneWall

---
+++ StoneWall/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoneWall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int solution(int[] H)
+        {
+            var stack = new Stack<int>();
+
+            int blocks = 0;
+
+            for (int i = 0; i < H.Length; i++)
+            {
+
+                while (stack.Count > 0 && stack.Peek() > H[i])
+                {
+                    stack.Pop();
+                }
+
+                if (stack.Count == 0)
+                {
+                    blocks++;
+                    stack.Push(H[i]);
+                }
+
+                if (stack.Peek() < H[i])
+                {
+                    blocks++;
+                    stack.Push(H[i]);
+                }
+            }
+
+            return blocks;
+        }
+    }
+}

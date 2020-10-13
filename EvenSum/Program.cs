9b17f462f7d9dc42bd8c93108284085f76500028
Yosheng <sad445566@gmail.com>
2020-10-13 23:23:26 +0800

    Reflecting EvenSum

--- EvenSum/Program.cs
+++ EvenSum/Program.cs
@@ -27,10 +27,11 @@ namespace EvenSum
             }
             
             Array.Sort(array);
-            return SubEvenSum(array, k, true);
-        }
 
-        static int SubEvenSum(int[] array, int k, bool even)
+            return subEvenSum(array, k, true);
+        }
+        
+        static int subEvenSum(int[] array, int k, bool isGetEven)
         {
             if (array.Length < k)
             {
@@ -39,14 +40,15 @@ namespace EvenSum
 
             if (k == 1)
             {
-                // 当 k==1 时，最大和就是最大的奇数或偶数
                 for (int i = array.Length - 1; i >= 0; i--)
                 {
-                    if (even && array[i] % 2 == 0)
+                    // 如果前面是偶数这里要取偶数
+                    if (isGetEven && array[i] % 2 == 0)
                     {
                         return array[i];
                     }
-                    else if (!even && array[i] % 2 == 1)
+
+                    if (!isGetEven && array[i] %2 == 1)
                     {
                         return array[i];
                     }
@@ -54,21 +56,41 @@ namespace EvenSum
 
                 return -1;
             }
-            /*  因为最后一个元素是假定被用上了的元素
-             *  1，2，3，4，5，假定5能被用上，那么再计算1，2，3，4的最大偶数或奇数和 
-             */
-            var subArray = new int[array.Length - 1];
-            Array.Copy(array, subArray, array.Length - 1 );
-            int prevEvenSum = SubEvenSum(subArray, k - 1, (array[array.Length - 1] % 2 == 0) == even);
 
-            if (prevEvenSum < 0)
+            // 最大数字
+            var cur = array[array.Length - 1];
+            
+            var subArray = new int[array.Length - 1];
+            Array.Copy(array, subArray, array.Length - 1);
+
+            var nextValue = subEvenSum(subArray, k - 1, cur % 2 == 0 == isGetEven);
+            if (nextValue > 0)
             {
-                return SubEvenSum(subArray, k, even);
+                return cur + nextValue;
             }
             else
             {
-                return array[array.Length - 1] + prevEvenSum;
+                return subEvenSum(subArray, k, isGetEven);
             }
+        }
+
+        static int subTotalSum(int[] array, int k)
+        {
+            var cur = 0;
+            
+            // 获取最大
+            cur = array[array.Length - 1];
+            
+            if (k == 1)
+            {
+                return cur;
+            }
+            
+            // 当前加上下一个获取
+            var subArray = new int[array.Length - 1];
+            Array.Copy(array, subArray, array.Length - 1);
+
+            return cur + subTotalSum(subArray, k - 1);
         }
     }
 }

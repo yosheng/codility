6342d4e084a97403fe0b907839ec34467c8daba7
Yosheng <yosheng0323@outlook.com>
2020-11-12 14:07:55 +0800

    Optmize EvenSum(Rec)

--- EvenSum/Program.cs
+++ EvenSum/Program.cs
@@ -27,10 +27,10 @@ namespace EvenSum
             }
             
             Array.Sort(array);
-            return SubEvenSum(array, k, true);
+            return subEvenSum(array, k, true);
         }
 
-        static int SubEvenSum(int[] array, int k, bool even)
+        static int subEvenSum(int[] array, int k, bool isGetEven)
         {
             if (array.Length < k)
             {
@@ -39,35 +39,39 @@ namespace EvenSum
 
             if (k == 1)
             {
-                // 当 k==1 时，最大和就是最大的奇数或偶数
-                for (int i = array.Length - 1; i >= 0; i--)
+                for (int i = array.Length -1 ; i >= 0; i--)
                 {
-                    if (even && array[i] % 2 == 0)
+                    // 如果前面总和是偶数这里必须取偶数确保总和偶数
+                    if (isGetEven && array[i] % 2 == 0)
                     {
                         return array[i];
                     }
-                    else if (!even && array[i] % 2 == 1)
+
+                    // 如果前面总和是奇数这里必须取奇数确保总和偶数
+                    if (!isGetEven && array[i] % 2 == 1)
                     {
                         return array[i];
                     }
                 }
-
+                
                 return -1;
             }
-            /*  因为最后一个元素是假定被用上了的元素
-             *  1，2，3，4，5，假定5能被用上，那么再计算1，2，3，4的最大偶数或奇数和 
-             */
+
+            var cur = array[array.Length - 1];
+            
             var subArray = new int[array.Length - 1];
             Array.Copy(array, subArray, array.Length - 1 );
-            int prevEvenSum = SubEvenSum(subArray, k - 1, (array[array.Length - 1] % 2 == 0) == even);
+            int prevEvenSum = subEvenSum(subArray, k - 1, cur % 2 == 0 == isGetEven);
 
+            // 判断下次要取的数字是否存在
             if (prevEvenSum < 0)
             {
-                return SubEvenSum(subArray, k, even);
+                // 不存在就从跳过这个数字重新取
+                return subEvenSum(subArray, k, isGetEven);
             }
             else
             {
-                return array[array.Length - 1] + prevEvenSum;
+                return cur + prevEvenSum;
             }
         }
     }
